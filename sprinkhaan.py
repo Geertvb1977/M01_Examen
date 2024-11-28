39a0c9d8f4a0549cc21c2d039c2398f2fa410189
Geertvb1977 <geert_vanbuggenhout@yahoo.com>
2024-11-28 21:45:56 +0100

    inlezen getal + test

--- sprinkhaan.py
+++ sprinkhaan.py
@@ -7,10 +7,32 @@ print("Maak sprongen naar links en naar rechts om Sonny thuis te brengen (naar 0
 print("Sonny heeft maar korte pootjes en kan dus maar kleine sprongetjes maken.")
 print("Maar pas op voor de wind.")
 
+zoek_Sonny = random.randint(-100, 100)   #Variabele 'zoek_Sonny' initialiseren en lees een random getal in
+
 Sonny_is_thuis = False
 
 while Sonny_is_thuis != True:
-    print("Sonny is er bijna")
+    try
+        sprong = input(int("Geef een sprong in (max 10) en een richting (- is links en + is rechts)"))
+        return sprong
+        break
+    except ValueError:
+            print("Dat was geen getal")    #(sys.exc_info()[1])
+            continue
+    
+    if sprong > 10: 
+        sprong = 10
+    elif sprong < -10:
+        sprong = -10
+    else:
+        continue
+
+    
+    
+
+
     Sonny_is_thuis = True
+
+print("Goed gedaan, bedankt om te spelen")
 
 
