3fd1d38b46bdb07e9e43595f3c3787c1f512716d
Geertvb1977 <geert_vanbuggenhout@yahoo.com>
2024-11-28 21:06:28 +0100

    Text

--- roovy.py
+++ roovy.py
@@ -1,1 +1,10 @@
+#Roovy de Mars roover
+print("Je moet de roover naar het laadstation brengen")
+print("Maar pas op! De batterij is bijna leeg")
+print("Beweeg de roover door instructies in te brengen")
+print("Je kan de roover bewegen door de volgende commando's in te geven")
+print("n voor noord, e voor oost, w voor west en s voor zuid")
+print("Geef ook het aantal stappen in")
+print("met c en een aantal batterij units kan je de batterij terug opladen met je zonnelader")
+print("Maar pas op! Opladen met de zon gaat heel traag")
 
