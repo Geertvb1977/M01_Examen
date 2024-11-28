179e30a3f38605664474c960dfc3763ba0d7b1e6
Geertvb1977 <geert_vanbuggenhout@yahoo.com>
2024-11-28 20:14:31 +0100

    Add files via upload

---
+++ sprinkhaan.py
@@ -0,0 +1,16 @@
+import random
+import sys
+
+print("Breng Sonny de sprinkhaan naar huis.")
+print("Sonny bevindt zich ergens in het gras en wil naar huis.")
+print("Maak sprongen naar links en naar rechts om Sonny thuis te brengen (naar 0).")
+print("Sonny heeft maar korte pootjes en kan dus maar kleine sprongetjes maken.")
+print("Maar pas op voor de wind.")
+
+Sonny_is_thuis = False
+
+while Sonny_is_thuis != True:
+    print("Sonny is er bijna")
+    Sonny_is_thuis = True
+
+
