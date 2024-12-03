f887ae203b71fa8e2f8773cd2d9d56f5b7cfe6b2
Geert Van Buggenhout <geert_vanbuggenhout@yahoo.com>
2024-12-03 22:04:41 +0100

    start while loop test

--- Grashopper_v2.py
+++ Grashopper_v2.py
@@ -3,3 +3,12 @@ import random
 
 position = random.randint(-100,100)
 print(f"initial position: {position}")
+
+while position not 0:
+    direction = input("Provide direction (l)eft or (r)ight: ")
+    print(f"direction: {direction}")
+    if direction not in ["l","r"]:
+        print("Goed gekozen")
+    else:
+        print("ongekende direction")
+
